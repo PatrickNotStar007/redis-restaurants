a00eebcab7815cd05b2af70c8658c0b4f71af684
Patrick <h.ilya2003@mail.ru>
2026-05-28 13:00:36 +0300

    express setup

---
+++ index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+
+import restaurantsRouter from "./routes/restaurants.js";
+import cuisinesRouterRouter from "./routes/cuisines.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+
+app.use("/restaurants", restaurantsRouter);
+app.use("/cuisine", cuisinesRouterRouter);
+
+app.use(errorHandler);
+
+app
+  .listen(PORT, () => {
+    console.log(`Приложение запущено на ${PORT} порту`);
+  })
+  .on("error", (error) => {
+    throw new Error(error.message);
+  });
