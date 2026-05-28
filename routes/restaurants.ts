3cbde24119c43d472881afa3372e7cf7019df586
Patrick <h.ilya2003@mail.ru>
2026-05-28 14:21:29 +0300

    zod and middleware

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -1,5 +1,11 @@
 import express from "express";
+import { validate } from "../middlewares/validate.js";
+import { RestaurantSchema, type Restaurant } from "../schemas/restaurant.js";
 
 const router = express.Router();
+
+router.post("/", validate(RestaurantSchema), async (req, res) => {
+  const data = req.body as Restaurant;
+});
 
 export default router;
